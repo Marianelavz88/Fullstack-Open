4d5ef36f8aa6fd70e5b0ec15c66fbb8a06666e4e
Marianelavz88 <Marianelavz88@gmail.com>
2024-10-08 16:56:28 -0300

    part1.1

---
+++ Part1/courseinfo/src/component/Content.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line react/prop-types
+const Content = ({part, exercise})  => {
+    return  (
+      <p>
+          {part} {exercise}
+        </p>
+    );
+    }
+    export default Content
---
+++ Part1/courseinfo/src/component/Header.jsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line react/prop-types
+const Header = ({course}) => {
+    return  <h1>{course}</h1>
+    }
+
+    export default Header
+    
